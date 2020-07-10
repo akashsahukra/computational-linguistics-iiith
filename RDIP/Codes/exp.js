801da71feea2dcd95a329f8ad91b712ba188e1a0
akashsahukara <akashsahukara@gmail.com>
2020-07-10 23:39:35 +0530

    4th commit

--- RDIP/Codes/exp.js
+++ RDIP/Codes/exp.js
@@ -1,5 +1,6 @@
-
-var s1=["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"]
+$(document).ready(function () {
+    
+var s1 = ["John ate an apple before afternoon", "before afternoon John ate an apple", "John before afternoon ate an apple"]
 var s2=["some students like to study in the night","at night some students like to study"]
 var s3=["John and Mary went to church","Mary and John went to church"]
 var s4=["John went to church after eating",	"after eating John went to church","John after eating went to church"]
@@ -8,17 +9,52 @@ var s6=["the woman who called my sister sells cosmetics","the woman who sells co
 var s7=["John goes to the library and studies","John studies and goes to the library"]
 var s8=["John ate an apple so did she","she ate an apple so did John"]
 var s9=["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"]
-var s10=["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her"	,"yesterday I bought a book that I told her"]
+var s10=["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her","yesterday I bought a book that I told her"]
 
 
 var sen1=["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"]
 var sen2=["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"]
 var sen3=["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"]
 var sen4=["राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम"]
-var sen5=["बिल्लियों को मारकर कुत्ता सो गया"	,"मारकर बिल्लियों को कुत्ता सो गया"	 ,"बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता"	,"कुत्ता सो गया बिल्लियों को मारकर"	,"कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"]
+var sen5=["बिल्लियों को मारकर कुत्ता सो गया","मारकर बिल्लियों को कुत्ता सो गया"	,"बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता"	,"कुत्ता सो गया बिल्लियों को मारकर"	,"कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"]
 var sen6=["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"]
-var sen7=["एक बड़ी सी किताब वहाँ है	","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब"," है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]
+var sen7=["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब"," है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]
 
+    function createRandom(RandEng) {
+        var idx = Math.floor(Math.random() * RandEng.length);
+        arrE = RandEng[idx];
+        var w_idx = Math.floor(Math.random() * arrE.length);
+        arr1 = arrE[w_idx].split(" ");
+        return arr1;
+    }    
+    function shuffle(arr1) {
+        var ctr = arr1.length, temp, index;
+
+        // While there are elements in the array
+        while (ctr > 0) {
+            // Pick a random index
+            index = Math.floor(Math.random() * ctr);
+            // Decrease ctr by 1
+            ctr--;
+            // And swap the last element with it
+            temp = arr1[ctr];
+            arr1[ctr] = arr1[index];
+            arr1[index] = temp;
+        }
+        return arr1;
+    }
+    function createButtons(list_of_buttons) {
+        for (let i = 0; i < list_of_buttons.length; i++) {
+            
+            var btn = document.createElement("button");
+            btn.id = i.toString();
+            btn.className = "word";
+            btn.textContent = list_of_buttons[i];
+            $("#btns").append(btn);
+            
+        }
+    }
+    
 $("#lang").on("change", function () {
     if ($(this).val() == "null") {
         alert("Select a language");
@@ -29,4 +65,21 @@ $("#lang").on("change", function () {
         $("#start-1").html("<p>" + "Form a sentence (Declarative or Interrogative or any other type) from the given words" + "</p>");
         $("#start-2").html("<p>" + "(select the buttons in proper order)" + "</p>");
     }
+    if ($(this).val() == "english") {
+        var RandEng = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10];
+        list_of_buttons = shuffle(createRandom(RandEng));
+        $("#btns").html("");
+        $("#btns").html(createButtons(list_of_buttons));
+        
+        
+    }
+
+    if ($(this).val() == "hindi") {
+        var RandHin = [sen1, sen2, sen3, sen4, sen5, sen6, sen7];
+        list_of_buttons1 = shuffle(createRandom(RandHin));
+        $("#btns").html("");
+        $("#btns").html(createButtons(list_of_buttons1));
+    }
+});
+
 });
