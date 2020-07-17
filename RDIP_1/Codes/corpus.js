6c9143fd891c32056bfbbea88354bb343681f604
akashsahukara <akashsahukara@gmail.com>
2020-07-17 18:34:15 +0530

    goal8

--- RDIP_1/Codes/corpus.js
+++ RDIP_1/Codes/corpus.js
@@ -5,6 +5,11 @@ var corpus2=['A wolf carried off a lamb. The lamb said, " I know you are going t
 
 var corpus3=["A man had a little dog, and he was very fond of it. He would pat its head, and take it on his knee, and talk to it. Then he would give it little bits of food from his own plate. A donkey looked in at the window and saw the man and the dog. \"Why does he not make a pet of me?\" said the donkey. \"It is not fair. I work hard, and the dog only wags its tail, and barks, and jumps on its master's knee. It is not fair.\" Then the donkey said to himself, \"If I do what the dog does, he may make a pet of me.\" So the donkey ran into the room. It brayed as loudly as it could. It wagged its tail so hard that it knocked over a jar on the table. Then it tried to jump on to its master's knee. The master thought the donkey was mad, and he shouted, \"Help! Help!\" Men came running in with sticks, and they beat the donkey till it ran out of the house, and they drove it back to the field. \"I only did what the dog does,\" said the donkey,\" and yet they make a pet of the dog, and they beat me with sticks. It is not fair."]
  
+let other_cases = new Map();
+other_cases.set(corpus1, ['not', 'as', 'heard', 'if', 'too']);
+other_cases.set(corpus2, ['at', 'better', 'he', 'heared', 'himself', 'insisted', 'once', 'would', 'than']);
+other_cases.set(corpus3, ['in', 'its', 'head', 'as', 'is', 'into', 'if', 'men', 'they', 'only']);
+
 var errmsg = document.getElementById("errmsg");
 var display = document.getElementById("paragraph");
 var inputs = document.getElementById("inputs");
@@ -15,11 +20,42 @@ var response = document.getElementById("response");
 var options = document.getElementById("options");
 
 //stemer
-var stemmer = new Snowball('English');
-stemmer.setCurrent('asky');    
-var s = stemmer.stem();
-console.log(s);
-console.log(stemmer.getCurrent());
+
+// stemmer.setCurrent('asky');    
+// var s = stemmer.stem();
+// console.log(s);
+// console.log(stemmer.getCurrent());
+
+function newtypes() {
+    corpval = document.getElementById("corpus").value;
+    temp = []
+    if (corpval == "corpus1") { 
+        c = corpus1[0];
+        console.log(c);
+        d = corpus1;
+    }
+    if (corpval == "corpus2") {
+        c = corpus2[0];
+        d = corpus2;
+    }
+    if (corpval == "corpus3") {
+        c = corpus3[0];
+        d = corpus3;
+    }
+    console.log(c);
+    c = c.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ").split(" ");
+    for (i = 0; i < c.length; i++){
+        arr = other_cases.get(d);
+
+        if (!arr.includes(c[i])) {
+            stemmer.setCurrent(c[i]);
+            stemmer.stem();
+            temp.push(stemmer.getCurrent());
+        }
+    }
+    count = counttypes(temp);
+    console.log(count);
+}
 
 function recalculate() {
     document.getElementById("re-calculate").style.display = "none";
@@ -27,7 +63,7 @@ function recalculate() {
     response.innerHTML = "<p>Now, consider all the tokens with the same 'root' word to be of the same type. Recalculate the number of types.</p>"
     document.getElementById("inp-3").innerHTML = '<p>#new types</p><input type="text" id="ans3" size="4"><br>';
     var another = document.getElementById("another-smt");
-    another.innerHTML = '<button id="re-submit">Submit</button>';
+    another.innerHTML = '<button id="re-submit" onclick="newtypes();">Submit</button>';
 }
 
 function tokens(str) {
@@ -41,7 +77,11 @@ function types(str) {
     ans = temp.filter(function (i, idx, arr) { return idx == arr.indexOf(i); });
     return ans.length;
 }
-
+function counttypes(arr) {
+    temp = arr.join('|').toLowerCase().split("|");
+    ans = temp.filter(function (i, idx, arr) { return idx == arr.indexOf(i); });
+    return ans.length;
+}
 
 function getAnswer(token, type) {
     if (token == inp1.value && type == inp2.value) {
@@ -89,10 +129,11 @@ function showcorpus() {
         response.innerHTML = "";
     
         
-        for (let i = 0; i < corpus.length; i++) {
-            display.append(corpus1[i]);
+        // for (let i = 0; i < corpus.length; i++) {
+        //     display.append(corpus1[i]);
             
-        }
+        // }
+        display.innerHTML = corpus1[0];
     }
     if (corpval == "corpus2") {
         display.innerHTML = "";
@@ -106,10 +147,11 @@ function showcorpus() {
         response.innerHTML = "";
         ;
         
-        for (let i = 0; i < corpus.length; i++) {
-            display.append(corpus2[i]);
+        // for (let i = 0; i < corpus.length; i++) {
+        //     display.append(corpus2[i]);
             
-        }
+        // }
+        display.innerHTML = corpus2[0];
     }
     if (corpval == "corpus3") {
         display.innerHTML = "";
@@ -123,9 +165,10 @@ function showcorpus() {
         response.innerHTML = "";
         
         
-        for (let i = 0; i < corpus.length; i++) {
-            display.append(corpus3[i]);
-        }
+        // for (let i = 0; i < corpus.length; i++) {
+        //     display.append(corpus3[i]);
+        // }
+        display.innerHTML = corpus3[0];
     }
     if(corpval == "none") {
         alert("Select a language");
@@ -161,3 +204,4 @@ function check() {
     }
 }
 
+// Source : https://stackoverflow.com/questions/13841217/remove-duplicate-characters-from-string/39761020
