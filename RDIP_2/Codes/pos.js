5f8d6f5bc1609a1a199dd2928d304550c6d12c7a
akashsahukara <akashsahukara@gmail.com>
2020-07-19 21:46:30 +0530

    goal4

--- RDIP_2/Codes/pos.js
+++ RDIP_2/Codes/pos.js
@@ -7,12 +7,16 @@ h1 = ['राम ने सीता के लिए फल तोड़ा।','�
         $('#language').html('');
         $("#lang").css("display", "block");
         $("#language").append('<option value=none>---Select a sentence---</option>')
-    for (let i = 0; i < arr.length; i++) {
-        $('#language').append('<option value='+i+'>'+arr[i]+'</option>');
+        for (let i = 0; i < arr.length; i++) {
+            
+            $('#language').append('<option value=' + arr[i]+'>'+arr[i]+'</option>');
     }
     }
     
     $("#pos").on('change', function () {
+        $("#pos-table").css("display", "none");
+        $("#table-pos").html('');
+        $("#stat").html("");
         if ($('#pos').val() == "eng") {
             show_drop_down(e1);
         }
@@ -23,5 +27,19 @@ h1 = ['राम ने सीता के लिए फल तोड़ा।','�
             alert("Select a language");
             return false;
         }
+
+    });
+
+    $("#language").on('change', function () {
+        $("#pos-table").css("display", "block");
+        $("#table-pos").html('');
+        $("#stat").html("<i>Select the POS tag for corresponding words</i>");
+        var val = $("#language option:selected").text().split(" ");
+        $("#table-pos").append('<tr><th>Lexicon</th><th>POS</th><th id="cor_head"></th><th id="ans_head"></th></tr>')
+        for (let i = 0; i < val.length; i++) {
+            $("#table-pos").append('<tr><td>'+val[i]+'</td><td><select name="t" id="token0"><option>Noun</option><option>Pronoun</option><option>Verb</option><option>Adjective</option><option>Adverb</option><option>Determiner</option><option>Preposition</option><option>Conjunction</option><option>Interjection</option></select></td><td id="correction0"></td><td id="correct0"></td></tr>')
+        }
+
+
     });
 });
