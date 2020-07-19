b88ec472fbfd82f8c0a826f252d4b3aed02c8747
akashsahukara <akashsahukara@gmail.com>
2020-07-19 19:55:55 +0530

    goal2

---
+++ RDIP_2/Codes/pos.js
@@ -0,0 +1,11 @@
+e1 = ['The child liked the chocolate.'] 
+e2 = ['She was stopped by the bravest knight.']
+e3 = ['Mary baked a cake for his birthday']
+e4 = ['She decorated the cake carefully']
+e5 = ['Mary wore a dress with polka dots.']
+
+h1 = ['राम ने सीता के लिए फल तोड़ा।']
+h2 = ['छोटे बच्चे पाठशाला जल्दी आयेंगे।']
+h3 = ['मेहनत का फल मीठा होता है।']
+h4 = ['वाह! वह खूबसूरत है।']
+h5 = ['पेड़ से पत्ते गिर गए।']
